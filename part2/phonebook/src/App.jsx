f64ad1c8ad72fad7b5a8f7e984d4bc5a70ff0829
ashoomky <ashlee@shum.co.nz>
2026-01-27 18:20:28 +1300

    part 2 chapter c - using json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  useEffect(() => {
+    console.log("effect")
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      console.log("promise fulfilled")
+      setPersons(response.data)
+    })
+  }, [])
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+  console.log(filter)
 
  
   return (
--- part2/phonebook/src/Filter.jsx
+++ part2/phonebook/src/Filter.jsx
@@ -3,6 +3,7 @@ const Filter = ({filter, setFilter}) => {
     const handleFilterChange = (event) => {
         setFilter(event.target.value)
     }
+    console.log(filter)
     return (
         <div>
             filter shown with <input value={filter} onChange = {handleFilterChange}/>
