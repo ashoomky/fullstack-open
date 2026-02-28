2f834d67845f1f9e79455da90f361256c676a4d1
ashoomky <ashlee@shum.co.nz>
2026-02-28 21:34:06 +1300

    ex.3.12 adding user to phonebook cli db

--- part3/phonebook-cli/mongo.js
+++ part3/phonebook-cli/mongo.js
@@ -1,13 +1,41 @@
 const mongoose = require('mongoose')
 
 const password = process.argv[2]
+const name = process.argv[3] 
+const added_password = process.argv[4]
 
-const url = `mongodb+srv://ashlee_db_user:${password}@phonebook.svnansv.mongodb.net/?appName=phonebook`
+const url = `mongodb+srv://ashlee_db_user:${password}@phonebook.svnansv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-mongoose.connect(url)
+mongoose.connect(url, {family: 4})
   .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
 })
+
+// schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    password: String
+})
+
+// model
+const Person = mongoose.model('Person', personSchema)
+
+// checking if name provided as argument in cli. if so, add to db. if not, list all entries
+if (name) {
+    const person = new Person({name: name, password: added_password})
+    person.save().then(result => {
+        console.log(`added ${name} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.password)
+        })
+        mongoose.connection.close()
+    })
+}
